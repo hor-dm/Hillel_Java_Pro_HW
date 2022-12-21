d222b240a1704435520747d35102461a96803175
eror2008 <d--g@ukr.net>
2022-12-21 18:52:40 +0200

    HW_06 was deleted

--- HW_06/ArrayDataException.java
+++
@@ -1,6 +0,0 @@
-public class ArrayDataException extends NumberFormatException {
-
-    public ArrayDataException(int i, int j) {
-        System.out.println("ArrayDataException says: Wrong array element, row: " + i + "  column: " + j);
-    }
-}
--- HW_06/ArraySizeException.java
+++
@@ -1,6 +0,0 @@
-public class ArraySizeException extends RuntimeException{
-
-    public ArraySizeException() {
-        System.out.println("ArraySizeException says: Wrong array size");
-    }
-}
